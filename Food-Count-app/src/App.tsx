b3740602c85425acab8cba823ca7211da27af442
AmrajKoonar <amrajk31@gmail.com>
2025-03-23 16:36:43 -0700

    feat: Edited DonorProfile.tsx -> DonorDashboard.tsx.

--- Food-Count-app/src/App.tsx
+++ Food-Count-app/src/App.tsx
@@ -3,7 +3,7 @@ import Layout from './components/layout/Layout';
 import Landing from './pages/Landing';
 import Signup from './pages/Signup';
 import Login from './pages/Login';
-import DonorProfile from './pages/Donor/DonorProfile';
+import DonorDashboard from './pages/Donor/DonorDashboard';
 
 
 // import RecipientProfile from './pages/Recipient/RecipientProfile'; for prianish when hes done his page
@@ -17,7 +17,7 @@ const App = () => {
           <Route index element={<Landing />} />
           <Route path="signup" element={<Signup />} />
           <Route path="login" element={<Login />} />
-          <Route path="/donor/profile" element={<DonorProfile />} />
+          <Route path="/donor/dashboard" element={<DonorDashboard />} />
 
         </Route>
       </Routes>
