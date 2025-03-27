b2e4f7363a799e77effa10ca6834b8b29b787c39
AmrajKoonar <amrajk31@gmail.com>
2025-03-27 15:33:27 -0700

    feat: Finished editlisting.tsx and updated app.tsx to fix routing for this page.

--- Food-Count-app/src/App.tsx
+++ Food-Count-app/src/App.tsx
@@ -6,6 +6,7 @@ import Login from './pages/Login';
 import DonorDashboard from './pages/Donor/DonorDashboard';
 import EditProfile from './pages/Donor/EditProfile';
 import NewListing from './pages/Donor/NewListing';
+import EditListing from './pages/Donor/EditListing';
 import RecipientDashboard from './pages/Recipient/RecipientDashboard';
 import RecipientProfile from './pages/Recipient/RecipientProfile';
 import RecipientReservations from './pages/Recipient/RecipientReservations';
@@ -24,6 +25,7 @@ const App = () => {
           <Route path="/donor/dashboard" element={<DonorDashboard />} />
           <Route path="/donor/profile" element={<EditProfile />} />
           <Route path="/donor/newlisting" element={<NewListing />} />
+          <Route path="/editlistings/:id" element={<EditListing />} />
           <Route path="/recipient/dashboard" element={<RecipientDashboard />} />
           <Route path="/recipient/profile" element={<RecipientProfile />} />
           <Route path="/recipient/reservations" element={<RecipientReservations />} />
--- Food-Count-app/src/pages/Donor/DonorDashboard.tsx
+++ Food-Count-app/src/pages/Donor/DonorDashboard.tsx
@@ -168,7 +168,7 @@ const DonorDashboard = () => {
                     
                     <div className="mt-6 grid grid-cols-2 gap-4">
                       <Link
-                        to={`/listings/editlisting/${listing.id}`}
+                        to={`/editlistings/${listing.id}`}
                         className="text-center py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500"
                       >
                         Edit
