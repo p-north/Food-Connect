36cd1d9b2971d594b8905bbd57aefd673ce8f7e8
p-north <puneetnorth170@gmail.com>
2025-06-10 13:42:18 -0700

    update s3 function

--- api/src/controllers/foodPost.controller.js
+++ api/src/controllers/foodPost.controller.js
@@ -1,18 +1,22 @@
 import {FoodPost} from "../models/foodPost.model.js";
-import { handleImageUpload, handleImageDeletion } from "../utils/amazonS3.utils.js";
+import { handleImageUpload, handleImageDeletion, generateSignedUrls } from "../utils/amazonS3.utils.js";
 
 const createFoodPost = async (req, res) => {
     try {
         // add userId to req.body (change this)
         req.body.userId = req.userID;
-        // generate signed image urls from s3 
-        const {urls} = await handleImageUpload(req, res);
-        console.log("AWS Urls", urls);
+        // generateimage keys from s3 
+        const {keys} = await handleImageUpload(req, res);
+        console.log("AWS keys", keys);
         
-        
-        // add the signed urls to body
-        req.body.imageUrl = urls;
+        // add the keys to body
+        req.body.imageUrl = keys;
         const data = await FoodPost.create(req.body);
+
+      
+        // Generate signed URLs for this post's images, and send data back
+        const signedUrlsResult = await generateSignedUrls(keys);
+        data.imageUrl = signedUrlsResult;
         res.status(201).json({
             success: true,
             data: data,
@@ -28,9 +32,30 @@ const createFoodPost = async (req, res) => {
 const getFoodPosts = async (req, res) => {
     try {
         const data = await FoodPost.findAll();
+
+        // for each post, convert keys into signed urls
+        const postsWithSignedUrls = await Promise.all(
+            data.map(async (post)=>{
+                if(post.imageUrl && post.imageUrl.length > 0){
+                    // Generate signed URLs for this post's images
+                    const signedUrlsResult = await generateSignedUrls(post.imageUrl);
+                    
+                    if (signedUrlsResult.success) {
+                        // Return post with signed URLs instead of keys
+                        return {
+                            ...post,
+                            imageUrl: signedUrlsResult.urls // Replace keys with signed URLs
+                        };
+                    }
+
+                }
+            })
+        );
+        
+    
         res.status(200).json({
             success: true,
-            data: data,
+            data: postsWithSignedUrls,
         });
     } catch (error) {
         res.status(500).json({
